4f61a0659d3321f700cedbb6cfd10d22a42e5549
WhereChosplums <bryanbiclaire@hotmail.fr>
2023-09-13 12:27:18 +0400

    card

---
+++ src/Pages/Card.jsx
@@ -0,0 +1,12 @@
+import Navbar from"../Components/Navbar"
+import Footer from"../Components/Footer"
+
+const Card = () => {
+    return (
+        <div className="Card"> 
+            
+            <Navbar />
+        </div>
+    );
+} 
+ export default Card; 
